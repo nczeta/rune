1adb1b81c3c79bd9e54c13be6b7b35ea22544738
nczeta <nc.zampini@gmail.com>
2026-08-24 21:09:10 +0200

    adding the memory so the user can conversate with the LLM model

--- main.py
+++ main.py
@@ -2,14 +2,29 @@ from groq import Groq
 
 client = Groq()
 
-response = client.chat.completions.create(
-    model="openai/gpt-oss-120b",
-    messages=[
+messages = []
+
+while True:
+    user_message = input("User: ")
+    messages.append(
         {
             "role": "user",
-            "content": "Ciao! Chi sei?"
+            "content": user_message
         }
-    ]
-)
+    )
 
-print(response.choices[0].message.content)
+    response = client.chat.completions.create(
+        model="openai/gpt-oss-120b",
+        messages=messages
+    )
+    print(response.choices[0].message.content)
+    
+    messages.append(
+        {
+            "role": "assistant",
+            "content": response.choices[0].message.content
+        }
+    )
+
+
+
