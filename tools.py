9cc4c24d5867b6d71d23f26866c7c02a49ceedfe
nczeta <nc.zampini@gmail.com>
2026-08-25 18:57:26 +0200

    added the get_github_following and get_github_followers

--- tools.py
+++ tools.py
@@ -25,3 +25,27 @@ def get_github_user(username):
         return error
 
     return r.json()
+
+
+def get_github_followers(username):
+    url = f"https://api.github.com/users/{username}/followers"
+    try:
+        r = requests.get(url)
+        r.raise_for_status
+    except requests.exceptions.HTTPError:
+        error = "error: invalid username"
+        return error
+
+    return r.json()
+
+
+def get_github_following(username):
+    url = f"https://api.github.com/users/{username}/following"
+    try:
+        r = requests.get(url)
+        r.raise_for_status
+    except requests.exceptions.HTTPError:
+        error = "error: invalid username"
+        return error
+
+    return r.json()
