20bb9997394f210ff5aecfc25b45c525062a4bd0
nczeta <nc.zampini@gmail.com>
2026-08-25 12:24:38 +0200

    created the tools description file and created the first description

---
+++ tools-description.py
@@ -0,0 +1,13 @@
+tools = [
+    {
+        "name": "get_github_repos",
+        "description": "Get the public repos from a provided GitHub user",
+        "parameters": {
+            "username": {
+                "type": "string",
+                "description": "The GitHub username"
+            }
+        },
+        "required": ["username"]
+    }
+]
