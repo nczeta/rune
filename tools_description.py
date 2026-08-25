d8f1f0dd2a693a7f0e228b1fdd97d81f02bcd349
nczeta <nc.zampini@gmail.com>
2026-08-25 18:53:03 +0200

    created the description of the get_github_user function

--- tools_description.py
+++ tools_description.py
@@ -17,9 +17,27 @@ tools_des = [
                 "required": ["username"]
             }
         }
+    },
+    {
+            "type": "function",
+            "function": {
+                "name": "get_github_user",
+                "description": "Get public information about a GitHub user, including their followers, following, profile details, and account dates.",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "username": {
+                            "type": "string",
+                            "description": "The GitHub username"
+                        }
+                    },
+                    "required": ["username"]
+                }
+            }
     }
 ]
 
 tool_functions = {
-    "get_github_repos": get_github_repos
+    "get_github_repos": get_github_repos,
+    "get_github_user": get_github_user
 }
