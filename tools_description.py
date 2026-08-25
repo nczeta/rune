a828c76ac64cb3837b82c277fbda4f5a3604a233
nczeta <nc.zampini@gmail.com>
2026-08-25 19:02:29 +0200

    added the description of the new tools

--- tools_description.py
+++ tools_description.py
@@ -34,10 +34,46 @@ tools_des = [
                     "required": ["username"]
                 }
             }
+    },
+    {
+                "type": "function",
+                "function": {
+                    "name": "get_github_following",
+                    "description": "Get public information about a GitHub user, including their followers, following, profile details, and account dates.",
+                    "parameters": {
+                        "type": "object",
+                        "properties": {
+                            "username": {
+                                "type": "string",
+                                "description": "The GitHub username"
+                            }
+                        },
+                        "required": ["username"]
+                    }
+                }
+    },
+    {
+                "type": "function",
+                "function": {
+                    "name": "get_github_followers",
+                    "description": "Get public information about a GitHub user, including their followers, following, profile details, and account dates.",
+                    "parameters": {
+                        "type": "object",
+                        "properties": {
+                            "username": {
+                                "type": "string",
+                                "description": "The GitHub username"
+                            }
+                        },
+                        "required": ["username"]
+                    }
+                }
     }
 ]
 
 tool_functions = {
     "get_github_repos": get_github_repos,
-    "get_github_user": get_github_user
+    "get_github_user": get_github_user,
+    "get_github_following": get_github_following,
+    "get_github_followers": get_github_followers
 }
