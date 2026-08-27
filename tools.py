1a84d03a85622d773454a65f17ec8ec9dfe991e9
nczeta <nc.zampini@gmail.com>
2026-08-27 19:03:21 +0200

    formatted the json datas

--- tools.py
+++ tools.py
@@ -188,6 +188,31 @@ def get_forecast(query):
             error = "error: couldn't get the forecast"
             return error
 
-    print(response.json())
-    
-get_forecast("Verona")
+    data = response.json()
+
+    current = data["current"]
+    daily = data["daily"]
+
+    forecast = {
+        "current": {
+            "time": current["time"],
+            "temperature": current["temperature_2m"],
+            "humidity": current["relative_humidity_2m"],
+            "apparent_temperature": current["apparent_temperature"],
+            "precipitation": current["precipitation"],
+            "weather_code": current["weather_code"],
+            "wind_speed": current["wind_speed_10m"]
+        },
+        "daily": []
+    }
+
+    for i in range(len(daily["time"])):
+        forecast["daily"].append({
+            "date": daily["time"][i],
+            "temperature_max": daily["temperature_2m_max"][i],
+            "temperature_min": daily["temperature_2m_min"][i],
+            "precipitation": daily["precipitation_sum"][i],
+            "weather_code": daily["weather_code"][i]
+        })
+
+    return forecast
