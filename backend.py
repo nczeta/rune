08a3e6badf368929b9292386e09f7dc6df087b64
nczeta <nc.zampini@gmail.com>
2026-09-14 17:56:27 +0200

    succesfully connected the backend to the rune website, now the application works perfectly

--- backend.py
+++ backend.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
 from main import run_rune
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 chats = {}
 
 class ChatRequest(BaseModel):
@@ -18,4 +27,10 @@ def chat(request: ChatRequest):
 
     return response
 
+
+def frontend():
+    return FileResponse("rune_v4.html")
+
+
 app.add_api_route("/chat", chat, methods=["POST"])
+app.add_api_route("/", frontend, methods=["GET"])
