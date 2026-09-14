44fcf0a75d182738ae2a286118dd8615ff57c92d
nczeta <nc.zampini@gmail.com>
2026-09-14 17:37:14 +0200

    connected the run_rune function to fastapi, now we can communicate with rune

--- backend.py
+++ backend.py
@@ -1,14 +1,21 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from main import run_rune
 
 app = FastAPI()
+chats = {}
 
 class ChatRequest(BaseModel):
+    session_id: str
     message: str
 
 
-def home():
-    return {"message": "RUNE API"}
+def chat(request: ChatRequest):
+    if not request.session_id in chats:
+        chats[request.session_id] = []
 
+    response, messages = run_rune(request.message, chats[request.session_id])
 
-app.add_api_route("/", home, methods=["GET"])
+    return response
+
+app.add_api_route("/chat", chat, methods=["POST"])
